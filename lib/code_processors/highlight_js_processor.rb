241f4f7e6f15dda0d3c0740d03c0dd71570211ea
Sam Rawlins <sam.rawlins@gmail.com>
2010-03-01 13:54:09 -0700

    Think it should be working...

--- lib/code_processors/highlight_js_processor.rb
+++ lib/code_processors/highlight_js_processor.rb
@@ -4,14 +4,11 @@ class CodeProcessors::HighlightJsProcessor < CodeProcessors::Base
   # * Radiant::Config['code.use_sns'] = 'true'
   # * sns extension is installed
   # * highlight.pack.js exists in the JS assets
-  # * each required highlight.js language exists in JS assets,
-  #   for example ruby.js
   # * each required highlight.js style exists in the CSS assets,
   #   for example github.css
   #
   # To avoid name conflicts with either highlight.js languages or styles, you
   # may specify a prefix for either. For example:
-  # * Radiant::Config['code.sns_language_prefix'] = 'hljs_'  # for hljs_ruby.js
   # * Radiant::Config['code.sns_style_prefix'] = 'hljs_'  # for hljs_github.css
   #
   # To allow for different highlight.pack.js filenames (for example, to support
@@ -33,12 +30,28 @@ class CodeProcessors::HighlightJsProcessor < CodeProcessors::Base
 
   # Include required javascripts
   def include_javascripts
-    '<script type="text/javascript" src="http://softwaremaniacs.org/media/soft/highlight/highlight.pack.js"></script><script type="text/javascript">hljs.tabReplace = \'    \';hljs.initHighlightingOnLoad();</script>'
+    s = ''
+    if useSNS?
+      script_name = Radiant::Config['code.sns_hljs_script'] || 'highlight.pack.js'
+      script = Javascript.find_by_name(script_name).url
+      s << '<script type="text/javascript" src="' + script + '"></script>'
+    else
+      s << '<script type="text/javascript" src="http://softwaremaniacs.org/media/soft/highlight/highlight.pack.js"></script>'
+    end
+    s << '<script type="text/javascript">hljs.tabReplace = \'    \';hljs.initHighlightingOnLoad();</script>'
   end
 
   # Include required stylesheets
   def include_stylesheets
-    '<link rel="stylesheet" title="highlight.js theme" href="http://softwaremaniacs.org/media/soft/highlight/styles/' + theme + '.css" />'
+    s = ''
+    if useSNS?
+      style_name  = Radiant::Config['code.sns_style_prefix'] || ''
+      style_name += theme + '.css'
+      style = Stylesheet.find_by_name(style_name).url
+      s << '<link rel="stylesheet" title="highlight.js theme" href="' + theme + '" />'
+    else
+      s << '<link rel="stylesheet" title="highlight.js theme" href="http://softwaremaniacs.org/media/soft/highlight/styles/' + theme + '.css" />'
+    end
   end
  
   protected
